a45574aa766473da4afa1c48f09ae4b6b0fd13d3
wendulem <34697277+wendulem@users.noreply.github.com>
2020-07-08 22:43:33 -0400

    responsive test

--- src/App.js
+++ src/App.js
@@ -5,22 +5,57 @@ import { ThemeProvider } from "./components/context/ThemeContext";
 import ScrollToTop from "./components/ScrollToTop";
 import Homepage from "./pages/Homepage";
 import Sales from "./pages/Sales";
+import DesktopBreakpoint from "./components/responsive_utilities/desktop_breakpoint";
+import TabletBreakpoint from "./components/responsive_utilities/tablet_breakpoint";
+import PhoneBreakpoint from "./components/responsive_utilities/phone_breakpoint";
 
 import "antd/dist/antd.css";
 
 function App() {
   return (
-    <ThemeProvider>
-      <Router>
-        <ScrollToTop>
-          <Route path="/" exact component={Homepage} />
-          <Switch>
-            <Route path="/home" exact component={Homepage} />
-            <Route path="/sales" exact component={Sales} />
-          </Switch>
-        </ScrollToTop>
-      </Router>
-    </ThemeProvider>
+    <div>
+      <DesktopBreakpoint>
+        <ThemeProvider>
+          <Router>
+            <ScrollToTop>
+              <Route path="/" exact component={Homepage} />
+              <Switch>
+                <Route path="/home" exact component={Homepage} />
+                <Route path="/sales" exact component={Sales} />
+              </Switch>
+            </ScrollToTop>
+          </Router>
+        </ThemeProvider>
+      </DesktopBreakpoint>
+
+      <PhoneBreakpoint>
+        <ThemeProvider>
+          <Router>
+            <ScrollToTop>
+              <Route path="/" exact component={Homepage} />
+              <Switch>
+                <Route path="/home" exact component={Homepage} />
+                <Route path="/sales" exact component={Sales} />
+              </Switch>
+            </ScrollToTop>
+          </Router>
+        </ThemeProvider>
+      </PhoneBreakpoint>
+
+      <TabletBreakpoint>
+        <ThemeProvider>
+          <Router>
+            <ScrollToTop>
+              <Route path="/" exact component={Homepage} />
+              <Switch>
+                <Route path="/home" exact component={Homepage} />
+                <Route path="/sales" exact component={Sales} />
+              </Switch>
+            </ScrollToTop>
+          </Router>
+        </ThemeProvider>
+      </TabletBreakpoint>
+    </div>
   );
 }
 
---
+++ src/components/responsive_utilities/phone_breakpoint.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Breakpoint from "./breakpoint";
+
+export default function PhoneBreakpoint(props) {
+  return <Breakpoint name="phone">{props.children}</Breakpoint>;
+}
